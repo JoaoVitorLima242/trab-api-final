72d40cdac7ac5590b336217c0961169d03c3ea51
JoaoVitorLima242 <jlima0500@gmail.com>
2023-06-20 19:30:21 -0300

    add postgress conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+const rootOptions: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '1234',
+  database: 'postgres',
+  autoLoadEntities: true,
+  synchronize: true,
+};
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, TypeOrmModule.forRoot(rootOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
