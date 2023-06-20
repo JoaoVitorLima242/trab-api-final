00668b0ccacdb7bf21ab0b3b81953e3925b3c849
JoaoVitorLima242 <jlima0500@gmail.com>
2023-06-20 19:38:49 -0300

    add tags relation

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -3,9 +3,10 @@ import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { Course } from './entities/courses.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from './entities/tags.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, Tag])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
